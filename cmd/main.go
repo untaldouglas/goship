14b63377504b05782eba60157b90fd407c0132ac
untaldouglas <galindo@untaldouglas.info>
2024-01-17 15:27:10 -0600

    test incorporated

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/untaldouglas/goship/handlers/rest"
 )
 
 func main() {
@@ -12,25 +13,9 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello",
-		func(w http.ResponseWriter, r *http.Request) {
-			enc := json.NewEncoder(w)
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			resp := Resp{
-				Language:    "English",
-				Translation: "Hello",
-			}
-			if err := enc.Encode(resp); err != nil {
-				panic("unable to encode response")
-			}
-		})
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
 
 	log.Printf("listening on %s\n", addr)
 
 	log.Fatal(http.ListenAndServe(addr, mux))
-}
-
-type Resp struct { // <6>
-	Language    string `json:"language"`
-	Translation string `json:"translation"`
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/untaldouglas/goship
+
+go 1.21.6
