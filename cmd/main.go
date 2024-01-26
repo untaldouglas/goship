198ca6f2e5b27ab1b8ad669e82f814559c7715e2
untaldouglas <galindo@untaldouglas.info>
2024-01-26 11:14:48 -0600

    Nuevo gcs

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/untaldouglas/goship/handlers"
 	"github.com/untaldouglas/goship/handlers/rest"
 )
 
@@ -14,7 +15,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
-
+	mux.HandleFunc("/health", handlers.HealthCheck)
+	
 	log.Printf("listening on %s\n", addr)
 
 	log.Fatal(http.ListenAndServe(addr, mux))
